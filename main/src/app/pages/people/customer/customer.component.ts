b31c2687a6708ed151057cb27bc7bd5cf5ef59ca
Raduanul Haque <raduanulhaque01@gmail.com>
2024-12-03 22:17:31 +0600

    angular update

--- main/src/app/pages/people/customer/customer.component.ts
+++ main/src/app/pages/people/customer/customer.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, DialogModule, ButtonModule,TableModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.scss'
 })
 export class CustomerComponent {
+  allCustomer:any;
+  newCustomer:any;
+
+  catDialogue:boolean = false;
+  branDialog:boolean = false;
 
 }
--- main/src/app/pages/people/supplier/supplier.component.ts
+++ main/src/app/pages/people/supplier/supplier.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-supplier',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, DialogModule, ButtonModule,TableModule],
   templateUrl: './supplier.component.html',
   styleUrl: './supplier.component.scss'
 })
 export class SupplierComponent {
+  allCustomer:any;
+  newCustomer:any;
 
+  catDialogue:boolean = false;
+  branDialog:boolean = false;
 }
